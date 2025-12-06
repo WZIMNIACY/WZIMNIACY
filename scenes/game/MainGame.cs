29f4b1bc5c39603dfc69c4e6a4a69594121c1f65
Sebastian <jsdthe1st@gmail.com>
2025-12-06 20:28:27 +0100

    Implemented turn and point system
    * initial commit
    
    * minor changes
    
    * added turn diodes
    
    * added player list turn highlighting
    
    * added getters, fixes
    
    * added scripts for subscenes

--- scenes/game/MainGame.cs
+++ scenes/game/MainGame.cs
@@ -1,19 +1,72 @@
 using Godot;
+using System;
+
+public enum Team
+{
+    Blue,
+    Red
+}
 
 public partial class MainGame : Control
 {
     [Export] Panel menuPanel;
+    [Export] ScoreContainer scoreContainerBlue;
+    [Export] ScoreContainer scoreContainerRed;
+    [Export] PlayerListContainer teamListBlue;
+    [Export] PlayerListContainer teamListRed;
     [Export] public RightPanel gameRightPanel;
     [Export] public CaptainInput gameInputPanel;
-
-    private bool isBlueTurn = true;
+    public const bool IsHost = true; // temp value
+    private int pointsBlue;
+    public int PointsBlue
+    {
+        get => pointsBlue;
+    }
+    private int pointsRed;
+    public int PointsRed
+    {
+        get => pointsRed;
+    }
+    Team startingTeam;
+    Team currentTurn;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
-	{
+    {
         base._Ready();
-        menuPanel = GetNode<Panel>("MenuPanel");
+
+        // Hide menu panel at start
         menuPanel.Visible = false;
+
+        // Choosing starting team
+        if (IsHost)
+        {
+            // Choose starting team randomly
+            startingTeam = (Team)Random.Shared.Next(0, 2);
+
+            // TODO: send starting team to clients
+        }
+        else
+        {
+            // TODO: wait to receive starting team from host
+        }
+
+        // Assing initianl points and turn
+        if (startingTeam == Team.Blue)
+        {
+            currentTurn = Team.Blue;
+            pointsBlue = 9;
+            pointsRed = 8;
+            SetTurnBlue();
+        }
+        else
+        {
+            currentTurn = Team.Red;
+            pointsBlue = 8;
+            pointsRed = 9;
+            SetTurnRed();
+        }
+        UpdatePointsDisplay();
 
         if (gameInputPanel != null)
         {
@@ -28,10 +81,10 @@ public partial class MainGame : Control
 
     private void StartCaptainPhase()
     {
-        GD.Print($"Początek tury {(isBlueTurn ? "BLUE" : "RED")}");
+        GD.Print($"Początek tury {(currentTurn == Team.Blue ? "BLUE" : "RED")}");
         if(gameInputPanel != null)
         {
-            gameInputPanel.SetupTurn(isBlueTurn);
+            gameInputPanel.SetupTurn(currentTurn == Team.Blue);
         }
     }
 
@@ -40,7 +93,7 @@ public partial class MainGame : Control
         GD.Print($"{word} [{number}]");
         if (gameRightPanel != null)
         {
-            gameRightPanel.UpdateHintDisplay(word, number, isBlueTurn);
+            gameRightPanel.UpdateHintDisplay(word, number, currentTurn == Team.Blue);
         }
     }
 
@@ -49,13 +102,13 @@ public partial class MainGame : Control
         GD.Print("Koniec tury");
         if(gameRightPanel != null)
             gameRightPanel.CommitToHistory();
-        isBlueTurn = !isBlueTurn;
+        TurnChange();
         StartCaptainPhase();
     }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
         base._Process(delta);
     }
 
@@ -95,5 +148,63 @@ public partial class MainGame : Control
     {
         GD.Print("ResumeButton pressed...");
         menuPanel.Visible = false;
+    }
+
+    private void UpdatePointsDisplay()
+    {
+        string textBlue = "Karty niebieskich: "; // temp value, czekam na lepsza propozycje od UI teamu
+        string textRed = "Karty czerwonych: "; // same
+        scoreContainerBlue.ChangeScoreText(textBlue + pointsBlue.ToString());
+        scoreContainerRed.ChangeScoreText(textRed + pointsRed.ToString());
+    }
+
+    public void RemovePointBlue()
+    {
+        GD.Print("Point removed from team blue...");
+        pointsBlue--;
+        UpdatePointsDisplay();
+        if (pointsBlue == 0)
+            EndGame(Team.Blue);
+    }
+
+    public void RemovePointRed()
+    {
+        GD.Print("Point removed from team red...");
+        pointsRed--;
+        UpdatePointsDisplay();
+        if (pointsRed == 0)
+            EndGame(Team.Red);
+    }
+
+    public void TurnChange()
+    {
+        if (currentTurn == Team.Blue)
+            SetTurnRed();
+        else
+            SetTurnBlue();
+    }
+
+    public void SetTurnBlue()
+    {
+        GD.Print("Turn blue...");
+        currentTurn = Team.Blue;
+        scoreContainerBlue.SetDiodeOn();
+        scoreContainerRed.SetDiodeOff();
+        teamListBlue.Modulate = new Color(2.8f, 2.8f, 2.8f, 1f);
+        teamListRed.Modulate = new Color(1f, 1f, 1f, 1f);
+    }
+
+    public void SetTurnRed()
+    {
+        GD.Print("Turn red...");
+        currentTurn = Team.Red;
+        scoreContainerBlue.SetDiodeOff();
+        scoreContainerRed.SetDiodeOn();
+        teamListBlue.Modulate = new Color(1f, 1f, 1f, 1f);
+        teamListRed.Modulate = new Color(2.8f, 2.8f, 2.8f, 1f);
+    }
+
+    public void EndGame(Team winner)
+    {
     }
 }
