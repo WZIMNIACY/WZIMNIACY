3e87e202c41965130e6dbe46b0656c94aa98dc8e
Adam5342 <s230983@sggw.edu.pl>
2025-12-10 21:01:23 +0100

    Wyswietlanie graczy na liscie

--- scripts/ui/PlayerListContainer.cs
+++ scripts/ui/PlayerListContainer.cs
@@ -6,10 +6,14 @@ public partial class PlayerListContainer : PanelContainer
 	
 	public enum PlayerTeam { None, Blue, Red }
 <<<<<<< HEAD
+<<<<<<< HEAD
 	[Export] public PlayerTeam team;
 =======
 	[Export] public PlayerTeam Team;
 >>>>>>> 4076424 (Wyswietlanie graczy na liscie)
+=======
+	[Export] public PlayerTeam team;
+>>>>>>> 5ef2641 (Wyswietlanie graczy na liscie)
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -32,10 +36,14 @@ public partial class PlayerListContainer : PanelContainer
 		foreach (var member in eosManager.GetCurrentLobbyMembers())
 		{
 <<<<<<< HEAD
+<<<<<<< HEAD
 			if((member["team"].ToString() == team.ToString()))
 =======
 			if((member["team"].ToString() == Team.ToString()))
 >>>>>>> 4076424 (Wyswietlanie graczy na liscie)
+=======
+			if((member["team"].ToString() == team.ToString()))
+>>>>>>> 5ef2641 (Wyswietlanie graczy na liscie)
 			{
 				foreach (Node child in playerListVBox.GetChildren())
 				{
