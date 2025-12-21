aa1017d0e10f7d0e7b355f7fd8c909d5588c9750
JSD <jsdthe1st@gmail.com>
2025-12-21 13:28:40 +0100

    initial commit

--- scenes/game/MainGame.cs
+++ scenes/game/MainGame.cs
@@ -97,6 +97,8 @@ public partial class MainGame : Control
         {
             GD.PrintErr("Error");
         }
+        
+
         EmitSignal(SignalName.GameReady);
     }
 
@@ -241,6 +243,35 @@ public partial class MainGame : Control
         scoreContainerRed.SetDiodeOn();
         teamListBlue.Modulate = new Color(1f, 1f, 1f, 1f);
         teamListRed.Modulate = new Color(2.8f, 2.8f, 2.8f, 1f);
+    }
+
+    public void CardConfirm(AgentCard card)
+    {
+        switch (card.Type)
+        {
+            case CardManager.CardType.Blue:
+                RemovePointBlue();
+                if (currentTurn == Team.Red)
+                    TurnChange();
+                break;
+
+            case CardManager.CardType.Red:
+                RemovePointRed();
+                if (currentTurn == Team.Blue)
+                    TurnChange();
+                break;
+
+            case CardManager.CardType.Common:
+                TurnChange();
+                break;
+
+            case CardManager.CardType.Assassin:
+                if (currentTurn == Team.Blue)
+                    EndGame(Team.Red);
+                else
+                    EndGame(Team.Blue);
+                break;
+        }
     }
 
     public void EndGame(Team winner)
