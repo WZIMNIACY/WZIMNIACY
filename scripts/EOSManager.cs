8f5ee7e826821ab08a46fec0c0107c925c75f5e6
Adam5342 <s230983@sggw.edu.pl>
2025-12-21 16:06:56 +0100

    Blokada przycisku skip w nie swojej turze

--- scripts/EOSManager.cs
+++ scripts/EOSManager.cs
@@ -2673,7 +2673,7 @@ public partial class EOSManager : Node
 		};
 	}
 
-	private Team GetTeamForUser(string userId)
+	public Team GetTeamForUser(string userId)
 	{
 		foreach (var member in currentLobbyMembers)
 		{
